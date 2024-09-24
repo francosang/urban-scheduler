5ac78b333643bd7be118ac88b77f1064896066b0
Franco Sangiacomo <franconecat@gmail.com>
2024-09-25 01:12:00 +0200

    Add another name check && add weekday check && refactor

--- urban_scheduler/main.py
+++ urban_scheduler/main.py
@@ -13,6 +13,10 @@ ignore_hours = [
     "20:00:00"
 ]
 
+ignore_weekdays = [
+    0, # Monday
+]
+
 def search_classes(day):
     resp = api.get_classes(day)
     classes = resp["data"]["classes"]
@@ -39,21 +43,52 @@ def book(classes):
         print("-----------     BOOKING     -----------")
         for c in classes:
             print_class(c)
-            api.book_class(c["id"])
+            print(c)
+            # api.book_class(c["id"])
 
 def print_summary(classes):
     for c in classes:
         print_class(c, include_booking=True)
 
 def filter_classes(classes):
-    # only Strenght classes
-    filtered = filter(lambda c: c["title"] == "Strenght", classes)
-    # only classes without booking
-    filtered = filter(lambda c: c["booking"] is None, filtered)
-    # only classes not in ignore_hours
-    filtered = filter(lambda c: c["startTime"] not in ignore_hours, filtered)
-    #  only classes not in ignore_classes
-    filtered = filter(lambda c: c["id"] not in ignore_classes, filtered)
+    def is_strenght(c):
+        return c["title"] == "Strenght" or c["title"] == "Strength"
+    
+    def has_free_spots(c):
+        return c["freeSpots"] > 0
+    
+    def is_not_booked(c):
+        return c["booking"] is None
+    
+    def is_not_in_ignore_hours(c):
+        return c["startTime"] not in ignore_hours
+    
+    def is_not_in_ignore_classes(c):
+        return c["id"] not in ignore_classes
+
+    def is_not_in_ignore_weekdays(c):
+        print("--------------------------")
+        start_date_time_utc = c["startDateTimeUTC"]
+
+        print("start_date_time_utc:", start_date_time_utc)
+
+        # Parse the date string into a datetime object
+        dt = datetime.fromisoformat(start_date_time_utc)
+
+        print("dt:", dt)
+
+        print("dt.weekday():", dt.weekday())
+        
+        # Check if the day is Monday (weekday() returns 0 for Monday)
+        return dt.weekday() not in ignore_weekdays
+    
+    filtered = filter(is_strenght, classes)
+    filtered = filter(has_free_spots, filtered)
+    filtered = filter(is_not_booked, filtered)
+    filtered = filter(is_not_in_ignore_hours, filtered)
+    filtered = filter(is_not_in_ignore_classes, filtered)
+    filtered = filter(is_not_in_ignore_weekdays, filtered)
+
     return list(filtered)
 
 def start():
